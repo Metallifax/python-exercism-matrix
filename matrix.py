e6e07e4aa2b0e135d759a28a01406397eb9a9dbe
Matt Forgione <mathew_forgione@msn.com>
2020-11-18 00:32:05 -0400

    learning a little list comprehension

--- matrix.py
+++ matrix.py
@@ -16,6 +16,19 @@ class Matrix:
     def get_str(self):
         return self.mat_string
 
+    def get_parsed_arr(self):
+        arr = self.get_arr()
+        parsed_arr = []
+        for i in range(len(arr)):
+            if arr[i] == int:
+                parsed_arr.append(arr[i])
+            else:
+                for j in range(len(parsed_arr)):
+                    if int(arr[i][j]):
+                        parsed_arr.append(arr[i][j])
+        return parsed_arr
+
+
     def row(self, index):
         pass
 
@@ -24,4 +37,4 @@ class Matrix:
 
 mat = Matrix('1 2 3 4\n5 6 7 8\n9 10 11 12')
 print(mat)
-print(mat.get_arr())
+print(mat.get_parsed_arr())
